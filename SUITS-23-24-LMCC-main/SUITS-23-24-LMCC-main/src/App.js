211bc8f5c5d637df1da37807fe7920824e98b61c
Moises Mata <123202308+moisesmata@users.noreply.github.com>
2024-02-15 21:59:29 -0500

    added work done by LMCC team

---
+++ SUITS-23-24-LMCC-main/SUITS-23-24-LMCC-main/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css'; 
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Tss from "./pages/tss";
+import Rover from "./pages/rover";
+import Rocks from "./pages/rocks";
+import Nav from "./pages/nav";
+import Ingress from "./pages/ingress";
+import Focus from "./pages/focus";
+import Equipment from "./pages/equipment";
+import Egross from "./pages/egross";
+import Constant from './pages/constant';
+
+
+function App() {
+  return (
+    <div>
+      <h1>CU SUITS</h1>
+
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/Constant" element={<Constant />} />
+          <Route path="/TSS" element={<Tss />} />
+          <Route path="/Rover" element={<Rover />} />
+          <Route path="/Rocks" element={<Rocks />} />
+          <Route path="/Nav" element={<Nav />} />
+          <Route path="/Ingress" element={<Ingress />} />
+          <Route path="/Focus" element={<Focus />} />
+          <Route path="/Equipment" element={<Equipment />} />
+          <Route path="/Egross" element={<Egross />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
